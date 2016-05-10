8aaf754dc3c783824ea70b889df81c7e6783dd2e
Aaron Pederson <aaron.pederson@gmail.com>
2016-05-10 14:01:04 -0700

    Updated tests to reflect package name.

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe 'ansible-php::default' do
-  describe package('php7.0-fpm') do
+describe 'ansible-php7::default' do
+  describe package('php7.0-json') do
     it { should be_installed.by('apt') }
   end
 end
